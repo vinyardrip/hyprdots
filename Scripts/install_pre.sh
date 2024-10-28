7b7db27092b13469b83209bcf54a7e4af0b64239
vinyardrip <vinyardrip@mail.ru>
2024-10-28 12:50:00 +0300

    change parallel download in pacman.conf

--- Scripts/install_pre.sh
+++ Scripts/install_pre.sh
@@ -76,7 +76,7 @@ if [ -f /etc/pacman.conf ] && [ ! -f /etc/pacman.conf.t2.bkp ]; then
     sudo cp /etc/pacman.conf /etc/pacman.conf.t2.bkp
     sudo sed -i "/^#Color/c\Color\nILoveCandy
     /^#VerbosePkgLists/c\VerbosePkgLists
-    /^#ParallelDownloads/c\ParallelDownloads = 5" /etc/pacman.conf
+    /^#ParallelDownloads/c\ParallelDownloads = 15" /etc/pacman.conf
     sudo sed -i '/^#\[multilib\]/,+1 s/^#//' /etc/pacman.conf
 
     sudo pacman -Syyu
